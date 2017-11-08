3282700bbf3e5b4dfbf07f02b1efd724f6a18e06
gale <najsnazan@gmail.com>
2017-11-08 01:54:52 +0100

    some frontend

--- src/main/java/com/crypto/arbitrage/configuration/WebSecurityConfig.java
+++ src/main/java/com/crypto/arbitrage/configuration/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home", "/register", "/webjars/**", "/css/**", "/js/**").permitAll()
+                .antMatchers("/", "/home", "/register", "/webjars/**", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
